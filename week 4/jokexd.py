252c794012e03417c755d1b14f14e002d4b35756
JeffBen4 <benjaminjeffreycoilacanaza@gmail.com>
2025-01-31 10:12:16 -0500

    ola bor

---
+++ week 4/jokexd.py
@@ -0,0 +1,29 @@
+
+
+
+
+def eat():
+    print("Eat Takis")
+
+def sleep():
+    print("Just 4 hours")
+
+def code():
+    print("With lofi music")
+
+def  RE4():
+    print("Again xd")
+
+def repeat():
+    print("Lo mismo pe :v")
+    
+
+alive = True
+while alive:
+    eat()
+    sleep()
+    code()
+    RE4()
+    repeat()
+
+
